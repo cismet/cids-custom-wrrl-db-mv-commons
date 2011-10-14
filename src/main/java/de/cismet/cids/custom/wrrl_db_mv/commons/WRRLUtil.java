4991923ce5f2fbe82554916e3e11c73a533f04be
Martin Scholl <martin.scholl@cismet.de>
2011-10-14 07:59:42 +0000

    [issue:2265] added common utilities

---
+++ src/main/java/de/cismet/cids/custom/wrrl_db_mv/commons/WRRLUtil.java
@@ -0,0 +1,29 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.cids.custom.wrrl_db_mv.commons;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   martin.scholl@cismet.de
+ * @version  $Revision$, $Date$
+ */
+public final class WRRLUtil {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String DOMAIN_NAME = "WRRL_DB_MV"; // NOI18N
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new WRRLUtil object.
+     */
+    private WRRLUtil() {
+    }
+}
